a291ce52f748d03188dbb0653ff8ddd2fce0bf38
Yash Jangir <yashja23@gmail.com>
2022-09-21 15:32:36 +0530

    Update index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="main-container">
       <header className="header">
-          <img alt = "meme generator logo" src = "/memeLogo.png" className="logo" />
+          <img alt = "meme generator logo" src = "https://www.kindpng.com/picc/m/410-4105079_decent-memes-png-logo-harry-potter-starbucks-transparent.png" className="logo" />
           <h1 className="heading">Meme generator</h1>
       </header>
       <Meme/>
